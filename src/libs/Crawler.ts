2195fd11fba9e74f309f62b6d5822376aa395717
Wargas Delmondes Teixeira <wargas.teixeira@sefin.fortaleza.ce.gov.br>
2024-09-05 08:25:27 -0300

    download

--- src/libs/Crawler.ts
+++ src/libs/Crawler.ts
@@ -1,24 +1,27 @@
 import EventEmitter from "eventemitter3";
 import type { Browser, Page } from "puppeteer-core";
 import puppeteer from "puppeteer-core";
+import { join } from 'path';
+import fs from 'fs';
 
 export class Crawler extends EventEmitter {
     browser: Browser = {} as Browser;
-    page: Page  = {} as Page;
+    page: Page = {} as Page;
 
-   
+    static tempFolder = join(__dirname, '..', 'temp')
+
     async connect() {
         try {
 
             this.browser = await puppeteer.connect({
                 browserURL: 'http://127.0.0.1:9222/json',
                 defaultViewport: null
-            }) 
-            
+            })
+
             this.emit(`browser:connected`, this.browser)
             this.page = await this.browser.newPage()
             this.emit(`browser:newPage`, this.page)
-        } catch(e) {
+        } catch (e) {
             console.log('error ao connecctar')
         }
     }
@@ -30,24 +33,62 @@ export class Crawler extends EventEmitter {
         return c;
     }
 
-    
+    async waitForDownload(): Promise<{saveAs: (to:string) => Promise<boolean>, suggestedFilename: string}> {
+        return new Promise(async (acept, reject) => {
+
+            const session = await this.page.createCDPSession()
+
+            await session.send('Browser.setDownloadBehavior', {
+                behavior: 'allowAndName',
+                downloadPath: Crawler.tempFolder,
+                eventsEnabled: true,
+            })
+
+            let suggestedFilename = ''
+
+            session.on('Browser.downloadWillBegin', ev => {
+                suggestedFilename = ev.suggestedFilename;
+            })
+            session.on('Browser.downloadProgress', ev => {
+                if (ev.state == 'completed') {
+                    acept({
+                        suggestedFilename,
+                        async saveAs(to:string) {
+                            return new Promise((accept, reject) => {
+                                try {
+                                    fs.copyFileSync(join(Crawler.tempFolder, ev.guid), to);
+                                    accept(true)
+                                } catch (error) {
+                                    reject(false)
+                                }
+                            })
+                        },                        
+                    })
+                }
+                if (ev.state == 'canceled') {
+                    reject(false)
+                }
+            })
+        })
+    }
+
     async waitForSelector(selector: string) {
         await this.page.waitForSelector(selector)
     }
 
-    async waitForFunction(pageFunction:any, ...args: any[]) {
+    async waitForFunction(pageFunction: any, ...args: any[]) {
         return new Promise(async acc => {
-            while(true) {
+            while (true) {
                 const value = await this.evaluate(pageFunction, ...args)
 
-                if(value) {
+                if (value) {
                     acc(value)
                     break;
                 }
             }
         })
     }
-  
+
     async goto(url: string) {
         await this.page.goto(url)
     }
@@ -56,29 +97,29 @@ export class Crawler extends EventEmitter {
         await this.page.waitForSelector(selector)
         await this.page.click(selector)
     }
-   
-    async type(selector: string, value:string) {
+
+    async type(selector: string, value: string) {
         await this.page.waitForSelector(selector)
         await this.page.type(selector, value)
     }
 
 
-    async select(selector: string, value:string) {
+    async select(selector: string, value: string) {
         await this.page.waitForSelector(selector)
         await this.page.select(selector, value)
     }
 
-  
-    async evaluate(pageFunction:any, ...args:any[]) {
+
+    async evaluate(pageFunction: any, ...args: any[]) {
         return this.page.evaluate(pageFunction, ...args)
     }
 
-    async evaluateBefore(selector: string, pageFunction:any, ...args: any[]) {
+    async evaluateBefore(selector: string, pageFunction: any, ...args: any[]) {
         await this.waitForSelector(selector)
         return this.evaluate(pageFunction, ...args)
     }
 
-   
+
     async sleep(time = 500) {
         return new Promise(acc => {
             setTimeout(() => {
@@ -87,7 +128,7 @@ export class Crawler extends EventEmitter {
         })
     }
 
-  
+
     async close() {
         this.page.close()
     }
